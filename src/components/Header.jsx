7fb5304d1fa90a43aae14b804106f91157a9392d
MatiasMartinez <matiaschino2012@gmail.com>
2024-07-10 01:23:00 -0300

    agregamos componente link a header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
 import image from "../assets/img/doguito.svg";
 import "../assets/css/componentes/header.css";
 
+//para arreglar el tema de la carga de las paginas 
+//usamos react-router-dom LINK  e intercambiamos por las etiquetas <a> para solucionar el problema
+//Y cambiar "href" por la prop "to"
+import { Link } from "react-router-dom";
+
 export const Header = () => {
   return (
     <header className="header container">
@@ -8,17 +13,17 @@ export const Header = () => {
             <span className="menu-hamburguer__icon"></span>
         </div>
         <div className="header-container">
-            <a href="/" className="flex flex--center">
+            <Link to="/" className="flex flex--center">
                 <img className="header__logo" src={image} alt="doguito"/>
                 <h1 className="header__title">Petshop</h1>
-            </a>
+            </Link>
         </div>
         <nav className="menu-header">
             <ul className="menu-items">
-                <li><a className="menu-item menu-item--entrar" href="#">Entrar</a></li>
-                <li><a className="menu-item" href="#">Productos</a></li>
-                <li><a className="menu-item" href="/">Blog</a></li>
-                <li><a className="menu-item" href="/sobre">Sobre</a></li>
+                <li><Link className="menu-item menu-item--entrar" to="#">Entrar</Link></li>
+                <li><Link className="menu-item" to="#">Productos</Link></li>
+                <li><Link className="menu-item" to="/">Blog</Link></li>
+                <li><Link className="menu-item" to="/sobre">Sobre</Link></li>
             </ul>
         </nav>
         <div className="menu-header-background"></div>
