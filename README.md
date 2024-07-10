80ee9b19f8fdfe0a21e91d259d0b699d73464930
MatiasMartinez <matiaschino2012@gmail.com>
2024-07-10 13:42:15 -0300

    clase terminado

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 # Lo que aprendimos en esta aula:
 
 
-- Mandamos el usuario para el componente 404 utilizando la función 'useNavigate()'.
-
-- Mejoramos la experiencia de buscar un artículo creando un filtro por categorías.
+- Trabajamos con rutas anidadas.
+- Vimos cómo crear una segunda camada de filtros para mostrar artículos de una subcategoría específica, mejorando la experiencia del usuario al buscar un artículo.
+- Usamos el hook useResolvedPath para crear caminos dinámicos.
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css';
 import { Header } from './components/Header';
+import Categoria from './pages/Categoria';
 import Home from './pages/Home';
 import { Page404 } from './pages/Page404';
 import { Post } from './pages/Post';
@@ -43,6 +44,7 @@ function App() {
           <Route path='/' element={<Home/>}/>
           <Route path='/sobre' element={<Sobre/>} />
           <Route path='/posts/:id' element={<Post/>} />
+          <Route path='/categoria/:id/*' element={<Categoria/>} />
           <Route path='*' element={<Page404/>}/>
         </Routes>
       </Router>
