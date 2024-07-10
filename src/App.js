4061d917aa8a6b4d8a2cc0b63e9eb39ab47d2e73
MatiasMartinez <matiaschino2012@gmail.com>
2024-07-10 01:08:14 -0300

    creamos navbar y terminamos clase 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css';
+import { Header } from './components/Header';
 import Home from './pages/Home';
 import { Page404 } from './pages/Page404';
 import Sobre from './pages/Sobre';
@@ -36,6 +37,7 @@ function App() {
   return (
     <>
       <Router>
+        <Header/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/sobre' element={<Sobre/>} />
